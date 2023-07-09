16094dc6b2f27c4ee1cf52648a17708724eabdd9
Mithun-mondol <mithun.mondol@gmail.com>
2023-07-09 18:39:06 +0600

    Second push

--- js/script.js
+++ js/script.js
@@ -34,6 +34,26 @@ $(function () {
 	});
 });
 
+// !...................... Background Video JavaScript ..............
 
-// !...................... Name JavaScript ..............
+window.onload = function () {
+	document.getElementById("video1").addEventListener("ended", function () {
+		document.getElementById("video2").play();
+		document.getElementById("video2").classList.remove("hide");
+		document.getElementById("video1").classList.add("hide");
+	});
+	document.getElementById("video2").addEventListener("ended", function () {
+		document.getElementById("video1").play();
+		document.getElementById("video1").classList.remove("hide");
+		document.getElementById("video2").classList.add("hide");
+	});
+};
 
+// !...................... Navigation Active class JavaScript ..............
+
+$(function () {
+	$(".menu_link").on("click", function () {
+		$(".menu_link").removeClass("active");
+		$(this).addClass("active");
+	});
+});
